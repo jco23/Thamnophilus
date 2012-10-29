b56916eb380aca4007cf4ab64160950c6ccd3466
jco23 <jmee.co@gmail.com>
2012-10-29 14:02:10 -0700

    Modified the Payment object to include the payment id.

--- src/com/thp/object/Payment.java
+++ src/com/thp/object/Payment.java
@@ -9,27 +9,48 @@ import java.util.*;
  * @author Mark Ma
  */
 public class Payment {
+    int paymentId;
     int invoiceId;
-   double amountPaid;
-    String paymentDate;
-    public Payment(int invId, double amtPaid, String date)
+    double amountPaid;
+    Date paymentDate;
+    
+    public Payment(){
+        setPayment(0,0,null);
+    }
+    public Payment(int invId, double amtPaid, Date date)
     {
-        invoiceId = invId;
-        amountPaid = amtPaid; 
-        paymentDate = date;
+        setPayment(invId, amtPaid, date);
+    }
+    public void setPayment(int invId, double amtPaid, Date pDate){
+        setInvoiceId(invId);
+        setAmountPaid(amtPaid);
+        setPaymentDate(pDate);
     }
     
-    public static String recordPayment(Payment pment)
-    {
-       boolean success = true; 
-       String status;
-       if(success == true) {
-            status = "Successful";
-        }     
-       else {
-            status = "Fail!";
-        }
-       return status;
+    public void setPaymentId(int payId){
+        paymentId = payId;
+    }
+    public void setInvoiceId(int invId){
+        invoiceId = invId;
+    }
+    public void setAmountPaid(double amtPaid){
+        amountPaid = amtPaid;
+    }
+    public void setPaymentDate(Date pDate){
+        paymentDate = pDate;
+    }
+    
+    public int getPaymentId(){
+        return paymentId;
+    }
+    public int getInvoiceId(){
+        return invoiceId;
+    }
+    public double getAmountPaid(){
+        return amountPaid;
+    }
+    public Date getPaymentDate(){
+        return paymentDate;
     }
     
 }
