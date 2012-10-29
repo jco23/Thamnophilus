0acce55900463654979eb41500005e3197252fc1
jco23 <jmee.co@gmail.com>
2012-10-29 14:02:42 -0700

    Modified the Person object to only include first name & last name.

--- src/com/thp/object/Person.java
+++ src/com/thp/object/Person.java
@@ -10,6 +10,7 @@ package com.thp.object;
  * @author Joanne Co
  */
 public class Person {
+    public static final int MAX_NAME = 30;
     String firstName;
     String lastName;
     
@@ -20,8 +21,21 @@ public class Person {
         setPerson(fn, ln);
     }
     public void setPerson(String fn, String ln){
-        firstName=fn;
-        lastName=ln;
+        if(fn.length() > MAX_NAME){
+            System.out.println("First name exceeds the maximum length. Will be truncated.");
+            firstName=fn.substring(0, MAX_NAME);
+        }
+        else{
+            firstName=fn;
+        }
+        
+        if(ln.length() > MAX_NAME){
+            System.out.println("Last name exceeds the maximum length. Will be truncated.");
+            lastName=fn.substring(0, MAX_NAME);
+        }
+        else{
+            lastName=ln;
+        }
     }
     
     public String getFirstName(){
