c21408abc95e2799bed5231dc3aa2572edd3ac7a
jco23 <jmee.co@gmail.com>
2012-10-29 14:00:07 -0700

    Moved in recordPayment logic in Payment object into the control function here.

--- src/com/thp/control/PaymentControl.java
+++ src/com/thp/control/PaymentControl.java
@@ -4,10 +4,23 @@
  */
 package com.thp.control;
 
+import com.thp.object.Payment;
+
 /**
  *
  * @author user
  */
 public class PaymentControl {
-    public static void enterPayment(){}  
+    public static String enterPayment(Payment pment){
+        boolean success = true; 
+        String status;
+        if(success == true) {
+            status = "Successful";
+        }     
+        else {
+            status = "Fail!";
+        }
+        return status;
+    } 
+   
 }
